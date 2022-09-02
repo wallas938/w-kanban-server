56ff336fa4b524192c6670ee8befbb044af987f7
Wallas9_3 <drame.sissako001@gmail.com>
2022-09-02 20:10:33 +0200

    mongodb database added

--- server.ts
+++ server.ts
@@ -1,15 +1,20 @@
 import express from "express";
-
+import * as dotenv from "dotenv";
+import mongoose from "mongoose";
+import { MongoClient, ServerApiVersion, MongoClientOptions } from "mongodb";
+dotenv.config();
 // TODO: Replace the following with your app's Firebase project configuration
 const app = express();
 // Initialize Firebase
+
 const port = process.env.PORT || 3000;
+const client = new MongoClient(`${process.env.URI}`);
 
 app.use(express.json());
 
-/* Routes */
-/* app.use("/auth", authRoutes); */
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(port, async () => {
+  client.connect((err) => {
+    console.log("Connection successful");
+    client.close();
+  });
 });
