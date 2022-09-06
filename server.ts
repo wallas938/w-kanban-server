8648b364340746fe7054d713ae6393a9b83aa5c6
wallas938 <drame.sissako001@gmail.com>
2022-09-06 17:24:31 +0200

    listening port changed

--- server.ts
+++ server.ts
@@ -5,7 +5,7 @@ import userRoutes from "./routes/user.routes";
 dotenv.config();
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(express.json());
 
