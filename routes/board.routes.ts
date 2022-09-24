304f58be5f4d3c35e0bd8596c9d2202c2d5e8ba9
Wallas9_3 <drame.sissako001@gmail.com>
2022-09-24 12:54:10 +0200

    editing task route added

--- routes/board.routes.ts
+++ routes/board.routes.ts
@@ -4,9 +4,10 @@ import boardController from "~/controllers/board.controller";
 const router = express.Router();
 
 router.post("/", boardController.postBoard);
-router.post("/:boardId", boardController.updateBoard);
+router.put("/:boardId", boardController.updateBoard);
 router.delete("/:boardId", boardController.deleteBoard);
 router.delete("/:boardId/columns/:columnIdx/tasks/:taskIdx", boardController.deleteTask);
+router.put("/:boardId/columns/:columnIdx/tasks/:taskIdx", boardController.updateTask);
 router.get("/", boardController.getBoards);
 
 export default router;
