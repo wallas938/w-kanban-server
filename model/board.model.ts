11a286ea75fb3eab3c27bc1a50d30a67c82c99a0
Wallas9_3 <drame.sissako001@gmail.com>
2022-10-19 02:05:01 +0200

    The required property of the column's model color property is set to false

--- model/board.model.ts
+++ model/board.model.ts
@@ -2,79 +2,79 @@ import mongoose, { Schema } from "mongoose";
 const BoardModel = mongoose.model(
   "Board",
   new Schema({
-      name: {
-        type: String,
-        required: true,
-      },
-      userId: {
-        type: String,
-        required: true,
-      },
-      columns: {
-        required: true,
-        type: [
-          {
-            id: {
-              type: String,
-              required: true,
-            },
-            name: {
-              type: String,
-              required: true,
-            },
-            color: {
-              type: String,
-              required: true,
-            },
-            tasks: {
-              required: false,
-              type: [
-                {
-                  id: {
-                    type: String,
-                    required: true,
-                  },
-                  title: {
-                    type: String,
-                    required: true,
-                  },
-                  description: {
-                    type: String,
-                    required: true,
-                  },
-                  status: {
-                    type: String,
-                    required: false,
-                  },
-                  columnId: {
-                    type: String,
-                    required: true,
-                  },
-                  subtasks: {
-                    required: false,
-                    type: [
-                      {
-                        id: {
-                          type: String,
-                          required: true,
-                        },
-                        title: {
-                          type: String,
-                          required: true,
-                        },
-                        isCompleted: {
-                          type: Boolean,
-                          required: true,
-                        },
-                      },
-                    ],
-                  },
-                },
-              ],
-            },
+    name: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+    columns: {
+      required: true,
+      type: [
+        {
+          id: {
+            type: String,
+            required: true,
           },
-        ],
-      },
+          name: {
+            type: String,
+            required: true,
+          },
+          color: {
+            type: String,
+            required: false,
+          },
+          tasks: {
+            required: false,
+            type: [
+              {
+                id: {
+                  type: String,
+                  required: true,
+                },
+                title: {
+                  type: String,
+                  required: true,
+                },
+                description: {
+                  type: String,
+                  required: true,
+                },
+                status: {
+                  type: String,
+                  required: false,
+                },
+                columnId: {
+                  type: String,
+                  required: true,
+                },
+                subtasks: {
+                  required: false,
+                  type: [
+                    {
+                      id: {
+                        type: String,
+                        required: true,
+                      },
+                      title: {
+                        type: String,
+                        required: true,
+                      },
+                      isCompleted: {
+                        type: Boolean,
+                        required: true,
+                      },
+                    },
+                  ],
+                },
+              },
+            ],
+          },
+        },
+      ],
+    },
   }
   )
 );
